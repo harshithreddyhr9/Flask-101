8286146604cc0f634281c9cd3c468e5f588bda4a
harshithreddyhr9 <shrshthreddy@gmail.com>
2018-12-24 19:41:21 -0800

    login validation done

--- Flask webapp/sportsblog/models.py
+++ Flask webapp/sportsblog/models.py
@@ -2,6 +2,11 @@ from datetime import datetime
 from sportsblog import db, login_manager
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 '''
 User table
 Id(Primary Key), username, email, image_file, password, posts
--- Flask webapp/sportsblog/routes.py
+++ Flask webapp/sportsblog/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from sportsblog import app, db, bcrypt
 from sportsblog.forms import RegistrationForm, LoginForm
 from sportsblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [
@@ -28,6 +29,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     
     if form.validate_on_submit():
@@ -42,11 +45,26 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first() #Get the username from the database
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
