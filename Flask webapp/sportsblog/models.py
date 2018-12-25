306e90a81eeb6663dce9a1de95c2ee8784032cdd
harshithreddyhr9 <shrshthreddy@gmail.com>
2018-12-24 19:20:19 -0800

    username and email validation added

--- Flask webapp/sportsblog/models.py
+++ Flask webapp/sportsblog/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from sportsblog import db
+from sportsblog import db, login_manager
+from flask_login import UserMixin
 
 '''
 User table
 Id(Primary Key), username, email, image_file, password, posts
 '''
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
